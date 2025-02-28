ffe9f45482b3fd91eeb58836bc7aedf1f9f02226
Mohanad ElAshrey <mohanadashrey2016@gmail.com>
2025-02-28 18:21:57 +0200

    made new login and register forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,32 @@
-<h1>LOGIN FORM</h1>
-<form action={{route('auth.login')}} method="POST">
-    @csrf
-    <input name='user_name' type="text" placeholder="username">
-    <input name='password' type="text" placeholder="password">
-    <input type="submit" value="submit">
-    @foreach ($errors->all() as $error)
-    <h1>{{ $error }}</h1>
-@endforeach
-</form>
+@extends('layouts.main')
+
+@section('title', 'Login')
+
+@section('main')
+
+<div class="container-fluid vh-100 vw-100 d-flex align-items-center justify-content-center">
+    <div class="w-25 border border-2 rounded p-3">
+        <h1 class="text-center">Log-in</h1>
+        <form action={{route('auth.login')}} method="POST">
+            @csrf
+            <div class="form-floating my-4">
+                <input type="text" class="form-control" id="username-input" placeholder="Enter Your Username." name="user_name">
+                <label for="username-input">Username</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="password-input" placeholder="Enter Your Password" name="password">
+                <label for="password-input">Password</label>
+            </div>
+            <input class="btn btn-primary" type="submit" value="Log-in">
+            @foreach ($errors->all() as $error)
+            <h1>{{ $error }}</h1>
+        @endforeach
+        </form>
+
+    </div>
+
+</div>
+
+@endsection
+
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,34 @@
-<h1>REGISTER FORM</h1>
-<form action={{route('auth.register')}} method="POST">
-    @csrf
-    <input name='user_name' type="text" placeholder="username">
-    <input name='user_password' type="password" placeholder="password">
-    <input name='user_password_confirmation' type="password" placeholder="password confirm">
-    <input type="submit" value="submit">
-    @foreach ($errors->all() as $error)
-    <h1>{{ $error }}</h1>
-@endforeach
-</form>
+@extends('layouts.main')
+
+@section('title', 'Create Account')
+
+@section('main')
+
+<div class="container-fluid vh-100 vw-100 d-flex align-items-center justify-content-center">
+    <div class="w-25 border border-2 rounded p-3">
+        <h1 class="text-center">Create New Account</h1>
+        <form action={{route('auth.register')}} method="POST">
+            @csrf
+            <div class="form-floating my-4">
+                <input type="text" class="form-control" id="new-username-input" placeholder="Choose Your Username." name="user_name">
+                <label for="new-username-input">Username</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="new-password-input" placeholder="Choose Your Password" name="user_password">
+                <label for="new-password-input">Password</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="new-password-input-confirm" placeholder="Confirm Your Password" name="user_password_confirmation">
+                <label for="new-password-input">Confirm Password</label>
+            </div>
+            <input class="btn btn-primary" type="submit" value="Create Your Account">
+            @foreach ($errors->all() as $error)
+            <h1>{{ $error }}</h1>
+        @endforeach
+        </form>
+
+    </div>
+
+</div>
+
+@endsection
